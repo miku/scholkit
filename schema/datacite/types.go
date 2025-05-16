650435aa87c1fe1ee7bb13b8530f6e33a0ec65e8
Martin Czygan <martin.czygan@gmail.com>
2025-05-16 17:51:05 +0200

    update datacite schema

--- schema/datacite/types.go
+++ schema/datacite/types.go
@@ -2,142 +2,133 @@ package datacite
 
 type Document struct {
 	Attributes struct {
-		CitationCount int64 `json:"citationCount"`
+		CitationCount int64 `json:"citationCount",omitempty`
 		Container     struct {
-		} `json:"container"`
-		ContentUrl   []string `json:"contentUrl"` // https://support.datacite.org/docs/api-get-doi
+			FirstPage      string `json:"firstPage",omitempty`
+			Identifier     string `json:"identifier",omitempty`
+			IdentifierType string `json:"identifierType",omitempty`
+			Issue          string `json:"issue",omitempty`
+			LastPage       string `json:"lastPage",omitempty`
+			Title          string `json:"title",omitempty`
+			Type           string `json:"type",omitempty`
+			Volume         string `json:"volume",omitempty`
+		} `json:"container",omitempty`
+		ContentUrl   []interface{} `json:"contentUrl",omitempty`
 		Contributors []struct {
-			Affiliation []struct {
-				AffiliationIdentifier       string `json:"affiliationIdentifier"`
-				AffiliationIdentifierScheme string `json:"affiliationIdentifierScheme"`
-				Name                        string `json:"name"`
-				SchemeUri                   string `json:"schemeUri"`
-			} `json:"affiliation"`
-			ContributorType string `json:"contributorType"`
-			FamilyName      string `json:"familyName"`
-			GivenName       string `json:"givenName"`
-			Name            string `json:"name"`
+			Affiliation     []interface{} `json:"affiliation",omitempty`
+			ContributorType string        `json:"contributorType",omitempty`
+			FamilyName      string        `json:"familyName",omitempty`
+			GivenName       string        `json:"givenName",omitempty`
+			Name            string        `json:"name",omitempty`
 			NameIdentifiers []struct {
-				NameIdentifier       string `json:"nameIdentifier"`
-				NameIdentifierScheme string `json:"nameIdentifierScheme"`
-				SchemeUri            string `json:"schemeUri"`
-			} `json:"nameIdentifiers"`
-
-			NameType string `json:"nameType"`
-		} `json:"contributors"`
-		Created  string `json:"created"`
+				NameIdentifier       string `json:"nameIdentifier",omitempty`
+				NameIdentifierScheme string `json:"nameIdentifierScheme",omitempty`
+				SchemeUri            string `json:"schemeUri",omitempty`
+			} `json:"nameIdentifiers",omitempty`
+			NameType string `json:"nameType",omitempty`
+		} `json:"contributors",omitempty`
+		Created  string `json:"created",omitempty`
 		Creators []struct {
-			Affiliation []struct {
-				AffiliationIdentifier       string `json:"affiliationIdentifier"`
-				AffiliationIdentifierScheme string `json:"affiliationIdentifierScheme"`
-				Name                        string `json:"name"`
-				SchemeUri                   string `json:"schemeUri"`
-			} `json:"affiliation"`
-
-			FamilyName      string `json:"familyName"`
-			GivenName       string `json:"givenName"`
-			Name            string `json:"name"`
-			NameIdentifiers []struct {
-				NameIdentifier       string `json:"nameIdentifier"`
-				NameIdentifierScheme string `json:"nameIdentifierScheme"`
-				SchemeUri            string `json:"schemeUri"`
-			} `json:"nameIdentifiers"`
-			NameType string `json:"nameType"`
-		} `json:"creators"`
-		// Dates []struct {
-		// 	Date     string `json:"date"`
-		// 	DateType string `json:"dateType"`
-		// } `json:"dates"`
-		// Descriptions []struct {
-		// 	Description     string `json:"description"`
-		// 	DescriptionType string `json:"descriptionType"`
-		// 	Lang            string `json:"lang"`
-		// } `json:"descriptions"`
-		DOI               string   `json:"doi"`
-		DownloadCount     int64    `json:"downloadCount"`
-		Formats           []string `json:"formats"`
+			Affiliation     []interface{} `json:"affiliation",omitempty`
+			FamilyName      string        `json:"familyName",omitempty`
+			GivenName       string        `json:"givenName",omitempty`
+			Name            string        `json:"name",omitempty`
+			NameIdentifiers []interface{} `json:"nameIdentifiers",omitempty`
+			NameType        string        `json:"nameType",omitempty`
+		} `json:"creators",omitempty`
+		Dates []struct {
+			Date            string      `json:"date",omitempty`
+			DateInformation interface{} `json:"dateInformation",omitempty`
+			DateType        string      `json:"dateType",omitempty`
+		} `json:"dates",omitempty`
+		Descriptions []struct {
+			Description     string `json:"description",omitempty`
+			DescriptionType string `json:"descriptionType",omitempty`
+			Lang            string `json:"lang",omitempty`
+		} `json:"descriptions",omitempty`
+		DOI               string   `json:"doi",omitempty`
+		DownloadCount     int64    `json:"downloadCount",omitempty`
+		Formats           []string `json:"formats",omitempty`
 		FundingReferences []struct {
-			AwardNumber          string `json:"awardNumber"`
-			AwardTitle           string `json:"awardTitle"`
-			FunderIdentifier     string `json:"funderIdentifier"`
-			FunderIdentifierType string `json:"funderIdentifierType"`
-			FunderName           string `json:"funderName"`
-		} `json:"fundingReferences"`
+			FunderName string `json:"funderName",omitempty`
+		} `json:"fundingReferences",omitempty`
 		GeoLocations []struct {
-			GeoLocationPlace string `json:"geoLocationPlace"`
-		} `json:"geoLocations"`
+			GeoLocationPlace string `json:"geoLocationPlace",omitempty`
+		} `json:"geoLocations",omitempty`
 		Identifiers []struct {
-			Identifier     string `json:"identifier"`
-			IdentifierType string `json:"identifierType"`
-		} `json:"identifiers"`
-		IsActive           bool        `json:"isActive"`
-		Language           string      `json:"language"`
-		MetadataVersion    int64       `json:"metadataVersion"`
-		PartCount          int64       `json:"partCount"`
-		PartOfCount        int64       `json:"partOfCount"`
-		PublicationYear    int64       `json:"publicationYear"`
-		Published          string      `json:"published"` // https://support.datacite.org/docs/api-get-doi
-		Publisher          string      `json:"publisher"`
-		Reason             interface{} `json:"reason"` // Legacy attribute for EZID compatibility.
-		ReferenceCount     int64       `json:"referenceCount"`
-		Registered         string      `json:"registered"`
+			Identifier     string `json:"identifier",omitempty`
+			IdentifierType string `json:"identifierType",omitempty`
+		} `json:"identifiers",omitempty`
+		IsActive           bool   `json:"isActive",omitempty`
+		Language           string `json:"language",omitempty`
+		MetadataVersion    int64  `json:"metadataVersion",omitempty`
+		PartCount          int64  `json:"partCount",omitempty`
+		PartOfCount        int64  `json:"partOfCount",omitempty`
+		PublicationYear    int64  `json:"publicationYear",omitempty`
+		Published          string `json:"published",omitempty`
+		Publisher          string `json:"publisher",omitempty`
+		Reason             string `json:"reason",omitempty`
+		ReferenceCount     int64  `json:"referenceCount",omitempty`
+		Registered         string `json:"registered",omitempty`
 		RelatedIdentifiers []struct {
-			RelatedIdentifier     string `json:"relatedIdentifier"`
-			RelatedIdentifierType string `json:"relatedIdentifierType"`
-			RelationType          string `json:"relationType"`
-		} `json:"relatedIdentifiers"`
+			RelatedIdentifier     string      `json:"relatedIdentifier",omitempty`
+			RelatedIdentifierType string      `json:"relatedIdentifierType",omitempty`
+			RelatedMetadataScheme interface{} `json:"relatedMetadataScheme",omitempty`
+			RelationType          string      `json:"relationType",omitempty`
+			ResourceTypeGeneral   interface{} `json:"resourceTypeGeneral",omitempty`
+			SchemeType            interface{} `json:"schemeType",omitempty`
+			SchemeUri             interface{} `json:"schemeUri",omitempty`
+		} `json:"relatedIdentifiers",omitempty`
 		RelatedItems []struct {
+			PublicationYear       string `json:"publicationYear",omitempty`
 			RelatedItemIdentifier struct {
-				RelatedItemIdentifier     string `json:"relatedItemIdentifier"`
-				RelatedItemIdentifierType string `json:"relatedItemIdentifierType"`
-			} `json:"relatedItemIdentifier"`
-			RelatedItemType string `json:"relatedItemType"`
-			RelationType    string `json:"relationType"`
+				RelatedItemIdentifier     string `json:"relatedItemIdentifier",omitempty`
+				RelatedItemIdentifierType string `json:"relatedItemIdentifierType",omitempty`
+			} `json:"relatedItemIdentifier",omitempty`
+			RelatedItemType string `json:"relatedItemType",omitempty`
+			RelationType    string `json:"relationType",omitempty`
 			Titles          []struct {
-				Title string `json:"title"`
-			} `json:"titles"`
-		} `json:"relatedItems"`
+				Title string `json:"title",omitempty`
+			} `json:"titles",omitempty`
+		} `json:"relatedItems",omitempty`
 		RightsList []struct {
-			Rights                 string `json:"rights"`
-			RightsIdentifier       string `json:"rightsIdentifier"`
-			RightsIdentifierScheme string `json:"rightsIdentifierScheme"`
-			RightsUri              string `json:"rightsUri"`
-			SchemeUri              string `json:"schemeUri"`
-		} `json:"rightsList"`
-		SchemaVersion string   `json:"schemaVersion"`
-		Sizes         []string `json:"sizes"`
-		Source        string   `json:"source"`
-		State         string   `json:"state"`
+			Rights string `json:"rights",omitempty`
+		} `json:"rightsList",omitempty`
+		SchemaVersion string   `json:"schemaVersion",omitempty`
+		Sizes         []string `json:"sizes",omitempty`
+		Source        string   `json:"source",omitempty`
+		State         string   `json:"state",omitempty`
 		Subjects      []struct {
-			Subject string `json:"subject"`
-		} `json:"subjects"`
+			Subject string `json:"subject",omitempty`
+		} `json:"subjects",omitempty`
 		Titles []struct {
-			Lang      string `json:"lang"`
-			Title     string `json:"title"`
-			TitleType string `json:"titleType"`
-		} `json:"titles"`
+			Lang      string `json:"lang",omitempty`
+			Title     string `json:"title",omitempty`
+			TitleType string `json:"titleType",omitempty`
+		} `json:"titles",omitempty`
 		Types struct {
-			Bibtex              string `json:"bibtex"`
-			Citeproc            string `json:"citeproc"`
-			ResourceTypeGeneral string `json:"resourceTypeGeneral"`
-			Ris                 string `json:"ris"`
-			SchemaOrg           string `json:"schemaOrg"`
-		} `json:"types"`
-		Updated        string `json:"updated"`
-		URL            string `json:"url"`
-		Version        string `json:"version"`
-		VersionCount   int64  `json:"versionCount"`
-		VersionOfCount int64  `json:"versionOfCount"`
-		ViewCount      int64  `json:"viewCount"`
-	} `json:"attributes"`
-	ID            string `json:"id"`
+			Bibtex              string `json:"bibtex",omitempty`
+			Citeproc            string `json:"citeproc",omitempty`
+			ResourceType        string `json:"resourceType",omitempty`
+			ResourceTypeGeneral string `json:"resourceTypeGeneral",omitempty`
+			Ris                 string `json:"ris",omitempty`
+			SchemaOrg           string `json:"schemaOrg",omitempty`
+		} `json:"types",omitempty`
+		Updated        string `json:"updated",omitempty`
+		Url            string `json:"url",omitempty`
+		Version        string `json:"version",omitempty`
+		VersionCount   int64  `json:"versionCount",omitempty`
+		VersionOfCount int64  `json:"versionOfCount",omitempty`
+		ViewCount      int64  `json:"viewCount",omitempty`
+	} `json:"attributes",omitempty`
+	Id            string `json:"id",omitempty`
 	Relationships struct {
 		Client struct {
 			Data struct {
-				Id   string `json:"id"`
-				Type string `json:"type"`
-			} `json:"data"`
-		} `json:"client"`
-	} `json:"relationships"`
-	Type string `json:"type"`
+				Id   string `json:"id",omitempty`
+				Type string `json:"type",omitempty`
+			} `json:"data",omitempty`
+		} `json:"client",omitempty`
+	} `json:"relationships",omitempty`
+	Type string `json:"type",omitempty`
 }
