d2375e5659778cba4cac53f19c02f666e432bff2
Martin Czygan <martin.czygan@gmail.com>
2025-05-23 18:08:11 +0200

    cluster: draft

--- cmd/sk-cluster/main.go
+++ cmd/sk-cluster/main.go
@@ -1,225 +1,341 @@
-// sk-cluster is a release entity clusterer. It takes release
-// entities and will group similar items together.
-//
-// $ zstdcat pile.jsonl.zst | sk-cluster -o catalog.jsonl.zst
+// todo: adjust code
 package main
 
 import (
 	"bufio"
-	"encoding/base32"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"runtime"
+	"regexp"
 	"strings"
-	"sync"
-
-	"github.com/miku/scholkit"
-	"github.com/miku/scholkit/normal"
-	"github.com/miku/scholkit/parallel"
-	"github.com/miku/scholkit/schema/fatcat"
+	"unicode"
 )
 
-var (
-	batchSize       = flag.Int("b", 20000, "batch size")
-	makeTable       = flag.Bool("T", false, "releases to tabular form")
-	includeBlob     = flag.Bool("I", false, "include source document as last column (base32 encoded)")
-	runGroupVerify  = flag.Bool("G", false, "group and run verification on a cluster")
-	groupFieldIndex = flag.Int("f", 0, "group by column given by index (starting at 1, like cut) ")
-	showVersion     = flag.Bool("version", false, "show version")
-)
-
-func main() {
-	flag.Parse()
-	if *showVersion {
-		fmt.Println(scholkit.Version)
-		os.Exit(0)
-	}
-	switch {
-	case *makeTable:
-		var C = normal.ReplaceNewlineAndTab // cleanup function for ids
-		var nzr = &normal.Pipeline{         // pipeline to clean titles
-			Normalizer: []normal.Normalizer{
-				&normal.Simple{},
-				&normal.RemoveWhitespace{},
-				&normal.LettersOnly{},
-			},
-		}
-		pp := parallel.NewProcessor(os.Stdin, os.Stdout, func(p []byte) ([]byte, error) {
-			var r fatcat.Release
-			if err := json.Unmarshal(p, &r); err != nil {
-				return nil, err
-			}
-			// tabularize data
-			fields := []string{
-				C(r.ID),
-				C(r.Source),
-				C(r.ExtIDs.Ark),
-				C(r.ExtIDs.Arxiv),
-				C(r.ExtIDs.Core),
-				C(r.ExtIDs.DBLP),
-				C(r.ExtIDs.DOI),
-				C(r.ExtIDs.FatcatReleaseIdent),
-				C(r.ExtIDs.FatcatWorkIdent),
-				C(r.ExtIDs.HDL),
-				C(r.ExtIDs.ISBN13),
-				C(r.ExtIDs.JStor),
-				C(r.ExtIDs.MAG),
-				C(r.ExtIDs.MID),
-				C(r.ExtIDs.OAI),
-				C(r.ExtIDs.OpenAlex),
-				C(r.ExtIDs.PII),
-				C(r.ExtIDs.PMCID),
-				C(r.ExtIDs.PMID),
-				C(r.ExtIDs.WikidataQID),
-				C(nzr.Normalize(r.Title)),
-			}
-			if *includeBlob {
-				// Running this took about 25min, result is 1TB uncompressed
-				// data; 230GB compressed. TODO: With base64 we could try to
-				// utility AVX2 with a library like
-				// https://github.com/segmentio/asm/, but base64 is probably
-				// dwarfed by json encoding.
-				encoded := base32.StdEncoding.EncodeToString(p)
-				fields = append(fields, encoded)
-			}
-			b := []byte(strings.Join(fields, "\t"))
-			b = append(b, '\n')
-			return b, nil
-		})
-		pp.BatchSize = *batchSize
-		if err := pp.Run(); err != nil {
-			log.Fatal(err)
-		}
-	case *runGroupVerify && *groupFieldIndex > 0:
-		// read line until we find all lines sharing the given key, then pass
-		// off to verification thread
-		// TODO: custom Split function, a mix of size (e.g. 16MB) and complete
-		// set of keys
-		// TODO: cannot use mmap, as we have a zstd file most of the time; we
-		// would need to have a "streaming zstd" file "szstd" that would write
-		// out a given number of bytes into a single zstd file and then
-		// concatenate multiple one into one single file; would need to fix
-		// some size upfront, e.g. 64MB chunks; then a 230GB file would contain
-		// about 4000 chunks; we could mmap the whole file and then let threads
-		// work on different parts simultaneously.
-		// TODO: for now, just iterate over stdin lines
-
-		// Setup goroutines
-		var (
-			queue   = make(chan [][]string)
-			resultC = make(chan []GroupResult)
-			done    = make(chan bool)
-			wg      sync.WaitGroup
-		)
-		// the fan-in goroutine
-		go writeWorker(resultC, done)
-		// start N worker threads
-		for i := 0; i < runtime.NumCPU(); i++ {
-			wg.Add(1)
-			go verifyWorker(queue, resultC, &wg)
-		}
-		// TODO: this is an inefficient way to get the key
-		keyFromLine := func(line string) string {
-			fields := strings.Split(line, "\t")
-			if *groupFieldIndex <= len(fields) {
-				return fields[*groupFieldIndex-1]
-			}
-			return ""
-		}
-		// Read from stdin, linewise; cf. https://gist.github.com/miku/2e1a9509527a547f6ffaf29e0b396de4
-		scanner := bufio.NewScanner(os.Stdin)
-		const bufSize = 512 * 1024 // increased, until we did not ran into overflows
-		var buf = make([]byte, bufSize)
-		scanner.Buffer(buf, bufSize)
-		var (
-			line, key    string
-			batch        [][]string // a list of list of lines sharing the same key
-			group        []string   // a list of lines sharing a key
-			currentKey   string
-			maxBatchSize = 1000000 // may be more lines
-		)
-		for scanner.Scan() {
-			line = scanner.Text()
-			key = keyFromLine(line)
-			if key == "" {
-				continue
-			}
-			if currentKey != "" && key != currentKey {
-				g := make([]string, len(group))
-				copy(g, group)
-				batch = append(batch, g)
-				group = nil
-			}
-			if len(batch) == maxBatchSize {
-				b := make([][]string, len(batch))
-				copy(b, batch)
-				queue <- b
-				batch = nil
-			}
-			group = append(group, line)
-			currentKey = key
-		}
-		// TODO: handle last batch
-		if scanner.Err() != nil {
-			log.Fatal(scanner.Err())
-		}
-		close(queue)
-		wg.Wait()
-		close(resultC)
-		<-done
-		log.Printf("done")
-	default:
-		log.Printf("use -T to create a table")
-	}
+type Contrib struct {
+	CreatorId string `json:"creator_id,omitempty"`
+	Extra     struct {
+		Seq             string   `json:"seq,omitempty"`
+		MoreAffiliation []string `json:"more_affiliation,omitempty"`
+	} `json:"extra,omitempty"`
+	GivenName      string `json:"given_name,omitempty"`
+	Index          int64  `json:"index,omitempty"`
+	RawName        string `json:"raw_name,omitempty"`
+	Role           string `json:"role,omitempty"`
+	Surname        string `json:"surname,omitempty"`
+	RawAffiliation string `json:"raw_affiliation,omitempty"`
 }
 
-type GroupResult struct {
-	// ID is some cluster id, we are not using that downstream just yet, so can
-	// be anything, really.
-	ID string `json:"id"`
-	// Releases are ids of releases which most likely describe the same thing.
-	Releases []string `json:"r"`
-	Size     int      `json:"size"`
-	Key      string   `json:"key"`
+type Ref struct {
+	ContainerName string          `json:"container_name,omitempty"`
+	Extra         json.RawMessage `json:"extra,omitempty"`
+	Index         int64           `json:"index,omitempty"`
+	Key           string          `json:"key,omitempty"`
+	Locator       string          `json:"locator,omitempty"`
+	Title         string          `json:"title,omitempty"`
+	Year          int64           `json:"year,omitempty"`
 }
 
-func verifyWorker(queue chan [][]string, resultC chan []GroupResult, wg *sync.WaitGroup) {
-	defer wg.Done()
-	// TODO: this is an inefficient way to get the key
-	keyFromLine := func(line string) string {
-		fields := strings.Split(line, "\t")
-		if *groupFieldIndex <= len(fields) {
-			return fields[*groupFieldIndex-1]
-		}
+type ExtID struct {
+	Ark                string `json:"ark,omitempty"`
+	Arxiv              string `json:"arxiv,omitempty"`
+	Core               string `json:"core,omitempty"`
+	DBLP               string `json:"dblp,omitempty"`
+	DOAJ               string `json:"doaj,omitempty"`
+	DOI                string `json:"doi,omitempty"`
+	FatcatReleaseIdent string `json:"release_ident,omitempty"`
+	FatcatWorkIdent    string `json:"work_ident,omitempty"`
+	HDL                string `json:"hdl,omitempty"`
+	ISBN13             string `json:"isbn13,omitempty"`
+	JStor              string `json:"jstor,omitempty"`
+	MAG                string `json:"mag,omitempty"`
+	MID                string `json:"mid,omitempty"`
+	OAI                string `json:"oai,omitempty"`
+	OpenAlex           string `json:"openalex,omitempty"`
+	PII                string `json:"pii,omitempty"`
+	PMCID              string `json:"pmcid,omitempty"`
+	PMID               string `json:"pmid,omitempty"`
+	WikidataQID        string `json:"wikidata_qid,omitempty"`
+}
+
+type Abstract struct {
+	Content  string `json:"content,omitempty"`
+	Mimetype string `json:"mimetype,omitempty"`
+	SHA1     string `json:"sha1,omitempty"`
+}
+
+type Release struct {
+	Source      string     `json:"source"`
+	Abstracts   []Abstract `json:"abstracts,omitempty"`
+	ContainerId string     `json:"container_id,omitempty"`
+	Contribs    []Contrib  `json:"contribs,omitempty"`
+	ExtIDs      ExtID      `json:"ext_ids,omitempty"`
+	Extra       struct {
+		Arxiv struct {
+			Subjects []string `json:"subjects,omitempty"`
+		} `json:"arxiv,omitempty"`
+		DBLP struct {
+			EE []struct {
+				Type string `json:"type,omitempty"`
+				Text string `json:"text,omitempty"`
+			} `json:"ee,omitempty"`
+		} `json:"dblp,omitempty"`
+		OpenAlex struct {
+			PdfUrls        []string `json:"pdf_urls,omitempty"`
+			LocationsCount int64    `json:"locations_count,omitempty"`
+			HasFulltext    bool     `json:"has_fulltext,omitempty"`
+			OpenAccess     struct {
+				AnyRepositoryHasFulltext bool        `json:"any_repository_has_fulltext,omitempty"`
+				IsOa                     bool        `json:"is_oa,omitempty"`
+				OaStatus                 string      `json:"oa_status,omitempty"`
+				OaUrl                    interface{} `json:"oa_url,omitempty"`
+			} `json:"open_access,omitempty"`
+		} `json:"openalex,omitempty"`
+		Crossref struct {
+			AlternativeId []string `json:"alternative-id,omitempty"`
+			Funder        []struct {
+				Award         []string `json:"award,omitempty"`
+				DOI           string
+				DOIAssertedBy string `json:"doi-asserted-by,omitempty"`
+				Name          string `json:"name,omitempty"`
+			} `json:"funder,omitempty"`
+			License []struct {
+				ContentVersion string `json:"content-version,omitempty"`
+				DelayInDays    int64  `json:"delay-in-days,omitempty"`
+				Start          string `json:"start,omitempty"`
+				URL            string
+			} `json:"license,omitempty"`
+			Subject []string `json:"subject,omitempty"`
+			Type    string   `json:"type,omitempty"`
+		} `json:"crossref,omitempty"`
+		OAI struct {
+			SetSpec []string `json:"set_spec,omitempty"`
+			URL     []string `json:"url,omitempty"`
+		} `json:"oai,omitempty"`
+	} `json:"extra,omitempty"`
+	Ident           string `json:"ident,omitempty"`
+	ID              string `json:"id,omitempty"`
+	Issue           string `json:"issue,omitempty"`
+	Language        string `json:"language,omitempty"`
+	LicenseSlug     string `json:"license_slug,omitempty"`
+	Number          string `json:"number,omitempty"`
+	OriginalTitle   string `json:"original_title,omitempty"`
+	Pages           string `json:"pages,omitempty"`
+	Publisher       string `json:"publisher,omitempty"`
+	Refs            []Ref  `json:"refs,omitempty"`
+	ReleaseDate     string `json:"release_date,omitempty"`
+	ReleaseStage    string `json:"release_stage,omitempty"`
+	ReleaseType     string `json:"release_type,omitempty"`
+	ReleaseYear     int64  `json:"release_year,omitempty"`
+	Revision        string `json:"revision,omitempty"`
+	State           string `json:"state,omitempty"`
+	Subtitle        string `json:"subtitle,omitempty"`
+	Title           string `json:"title,omitempty"`
+	Version         string `json:"version,omitempty"`
+	Volume          string `json:"volume,omitempty"`
+	WithdrawnStatus string `json:"withdrawn_status,omitempty"`
+	WithdrawnDate   string `json:"withdrawn_date,omitempty"`
+	WithdrawnYear   string `json:"withdrawn_year,omitempty"`
+	WorkID          string `json:"work_id,omitempty"`
+}
+
+// Normalize title for clustering - lowercase, remove/replace non-letter chars, trim whitespace
+func normalizeTitle(title string) string {
+	if title == "" {
 		return ""
 	}
-	for batch := range queue {
-		var result []GroupResult
-		for _, g := range batch {
-			key := keyFromLine(g[0])
-			gr := GroupResult{
-				ID:       "dummy",
-				Releases: []string{"x", "y", "z"},
-				Size:     len(g),
-				Key:      key,
-			}
-			result = append(result, gr)
-		}
-		resultC <- result
-	}
+
+	// Convert to lowercase
+	normalized := strings.ToLower(title)
+
+	// Replace sequences of non-letter/non-digit characters with single space
+	re := regexp.MustCompile(`[^a-z0-9]+`)
+	normalized = re.ReplaceAllString(normalized, " ")
+
+	// Trim whitespace
+	normalized = strings.TrimSpace(normalized)
+
+	return normalized
 }
 
-func writeWorker(resultC chan []GroupResult, done chan bool) {
-	bw := bufio.NewWriter(os.Stdout)
-	defer bw.Flush()
-	enc := json.NewEncoder(bw)
-	for grs := range resultC {
-		for _, gr := range grs {
-			_ = enc.Encode(gr)
+// Remove all non-ASCII letters and digits, replace with nothing
+func normalizeStrict(title string) string {
+	if title == "" {
+		return ""
+	}
+
+	var result strings.Builder
+	for _, r := range strings.ToLower(title) {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			if r <= 127 { // ASCII only
+				result.WriteRune(r)
+			}
 		}
 	}
-	done <- true
+	return result.String()
+}
+
+// Create a sortable key from words (for clustering)
+func createSortedKey(title string) string {
+	if title == "" {
+		return ""
+	}
+
+	words := strings.Fields(normalizeTitle(title))
+	if len(words) == 0 {
+		return ""
+	}
+
+	// Sort words to handle word order variations
+	for i := 0; i < len(words); i++ {
+		for j := i + 1; j < len(words); j++ {
+			if words[i] > words[j] {
+				words[i], words[j] = words[j], words[i]
+			}
+		}
+	}
+
+	return strings.Join(words, "")
+}
+
+func main() {
+	// Command line flags
+	var (
+		inputFile  = flag.String("i", "", "input NDJSON file (default: stdin)")
+		outputFile = flag.String("o", "", "output file (default: stdout)")
+		separator  = flag.String("s", ";", "field separator (default: semicolon)")
+		useTab     = flag.Bool("tab", false, "use tab as separator (overrides -s)")
+	)
+	flag.Parse()
+
+	// Determine separator
+	sep := *separator
+	if *useTab {
+		sep = "\t"
+	}
+
+	// Open input (stdin or file)
+	var input *os.File
+	var err error
+
+	if *inputFile == "" {
+		input = os.Stdin
+	} else {
+		input, err = os.Open(*inputFile)
+		if err != nil {
+			log.Fatal("Error opening input file:", err)
+		}
+		defer input.Close()
+	}
+
+	var w *os.File
+	var bw = bufio.NewWriter(w)
+	defer bw.Flush()
+
+	if *outputFile == "" {
+		w = os.Stdout
+	} else {
+		w, err = os.Create(*outputFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer w.Close()
+	}
+
+	header := []string{
+		"id",
+		"source",
+		"title",
+		"subtitle",
+		"original_title",
+		"title_normalized",
+		"title_strict",
+		"title_sorted_key",
+		"release_year",
+		"release_type",
+		"publisher",
+		"language",
+		"doi",
+		"arxiv",
+		"pmid",
+		"pmcid",
+		"isbn13",
+		"hdl",
+		"ark",
+		"openalex",
+		"dblp",
+		"wikidata_qid",
+		"first_author",
+	}
+	fmt.Fprintln(w, strings.Join(header, sep))
+
+	scanner := bufio.NewScanner(input)
+	lineNum := 0
+
+	for scanner.Scan() {
+		lineNum++
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		var release Release
+		if err := json.Unmarshal([]byte(line), &release); err != nil {
+			log.Printf("Error parsing line %d: %v", lineNum, err)
+			continue
+		}
+
+		firstAuthor := ""
+		if len(release.Contribs) > 0 {
+			firstAuthor = release.Contribs[0].RawName
+			if firstAuthor == "" && release.Contribs[0].GivenName != "" && release.Contribs[0].Surname != "" {
+				firstAuthor = release.Contribs[0].GivenName + " " + release.Contribs[0].Surname
+			}
+		}
+
+		titleNormalized := normalizeTitle(release.Title)
+		titleStrict := normalizeStrict(release.Title)
+		titleSortedKey := createSortedKey(release.Title)
+
+		row := []string{
+			release.ID,
+			release.Source,
+			release.Title,
+			release.Subtitle,
+			release.OriginalTitle,
+			titleNormalized,
+			titleStrict,
+			titleSortedKey,
+			fmt.Sprintf("%d", release.ReleaseYear),
+			release.ReleaseType,
+			release.Publisher,
+			release.Language,
+			release.ExtIDs.DOI,
+			release.ExtIDs.Arxiv,
+			release.ExtIDs.PMID,
+			release.ExtIDs.PMCID,
+			release.ExtIDs.ISBN13,
+			release.ExtIDs.HDL,
+			release.ExtIDs.Ark,
+			release.ExtIDs.OpenAlex,
+			release.ExtIDs.DBLP,
+			release.ExtIDs.WikidataQID,
+			firstAuthor,
+		}
+		for i, field := range row {
+			field = strings.ReplaceAll(field, "\n", " ")
+			field = strings.ReplaceAll(field, "\r", " ")
+			if sep != "\t" {
+				field = strings.ReplaceAll(field, sep, " ")
+			} else {
+				field = strings.ReplaceAll(field, "\t", " ")
+			}
+			row[i] = field
+		}
+		fmt.Fprintln(w, strings.Join(row, sep))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
