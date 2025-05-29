7dc9746354e72afc3853a722dbe9db743048f67c
Martin Czygan <martin.czygan@gmail.com>
2025-05-29 18:32:21 +0200

    convert: more tests

--- convert/convert_test.go
+++ convert/convert_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/miku/scholkit/schema/crossref"
+	"github.com/miku/scholkit/schema/datacite"
 )
 
 func TestConvertCrossref(t *testing.T) {
@@ -28,6 +29,50 @@ func TestConvertCrossref(t *testing.T) {
 				t.Fatal(err)
 			}
 			release, err := CrossrefWorkToFatcatRelease(&work)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got, err := json.MarshalIndent(release, "", "    ")
+			if err != nil {
+				t.Fatal(err)
+			}
+			goldenfile := filepath.Join("testdata", name+".golden")
+			want, err := os.ReadFile(goldenfile)
+			if err != nil {
+				if os.IsNotExist(err) {
+					if err := os.WriteFile(goldenfile, got, 0644); err != nil {
+						t.Fatal(err)
+					}
+					t.Logf("created golden file: %s", goldenfile)
+					return
+				}
+				t.Fatal(err)
+			}
+			if string(got) != string(want) {
+				t.Errorf("%s: got: %s want: %s", name, string(got), string(want))
+			}
+		})
+	}
+}
+
+func TestConvertDatacite(t *testing.T) {
+	paths, err := filepath.Glob(filepath.Join("testdata", "datacite-*.input"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, path := range paths {
+		base := filepath.Base(path)
+		name := strings.TrimSuffix(base, filepath.Ext(base))
+		t.Run(name, func(t *testing.T) {
+			b, err := os.ReadFile(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var doc datacite.Document
+			if err := json.Unmarshal(b, &doc); err != nil {
+				t.Fatal(err)
+			}
+			release, err := DataCiteToFatcatRelease(&doc)
 			if err != nil {
 				t.Fatal(err)
 			}
