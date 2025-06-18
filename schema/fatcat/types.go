347830cb7d41d887a1dc3820df739f5a4679f766
Martin Czygan <martin.czygan@gmail.com>
2025-06-18 17:12:36 +0200

    match: fatcat/api/v1models.py

--- schema/fatcat/types.go
+++ schema/fatcat/types.go
@@ -2,8 +2,17 @@ package fatcat
 
 import "encoding/json"
 
+type Creator struct {
+	DisplayName string `json:"display_name,omitempty"`
+	GivenName   string `json:"given_name,omitempty"`
+	Surname     string `json:"surname,omitempty"`
+	Orcid       string `json:"orcid,omitempty"`
+	WikidataQid string `json:"wikidata_qid,omitempty"`
+}
+
 type Contrib struct {
-	CreatorId string `json:"creator_id,omitempty"`
+	CreatorId string   `json:"creator_id,omitempty"`
+	Creator   *Creator `json:"creator,omitempty"`
 	Extra     struct {
 		Seq             string   `json:"seq,omitempty"`
 		MoreAffiliation []string `json:"more_affiliation,omitempty"`
@@ -17,13 +26,14 @@ type Contrib struct {
 }
 
 type Ref struct {
-	ContainerName string          `json:"container_name,omitempty"`
-	Extra         json.RawMessage `json:"extra,omitempty"`
-	Index         int64           `json:"index"`
-	Key           string          `json:"key,omitempty"`
-	Locator       string          `json:"locator,omitempty"`
-	Title         string          `json:"title,omitempty"`
-	Year          int64           `json:"year,omitempty"`
+	ContainerName   string          `json:"container_name,omitempty"`
+	Extra           json.RawMessage `json:"extra,omitempty"`
+	Index           int64           `json:"index,omitempty"` // todo: need pointer?
+	Key             string          `json:"key,omitempty"`
+	Locator         string          `json:"locator,omitempty"`
+	Title           string          `json:"title,omitempty"`
+	Year            *int64          `json:"year,omitempty"`
+	TargetReleaseId string          `json:"target_release_id,omitempty"`
 }
 
 type ExtID struct {
@@ -52,15 +62,20 @@ type Abstract struct {
 	Content  string `json:"content,omitempty"`
 	Mimetype string `json:"mimetype,omitempty"`
 	SHA1     string `json:"sha1,omitempty"`
+	Lang     string `json:"lang,omitempty"`
 }
 
 // Release, with expanded entities, from v0 API.
 type Release struct {
-	Source      string     `json:"source"` // source name; uniq; newly added
-	Abstracts   []Abstract `json:"abstracts,omitempty"`
-	ContainerId string     `json:"container_id,omitempty"`
-	Contribs    []Contrib  `json:"contribs,omitempty"`
-	ExtIDs      ExtID      `json:"ext_ids,omitempty"`
+	Source      string                   `json:"source"` // source name; uniq; newly added
+	Abstracts   []Abstract               `json:"abstracts,omitempty"`
+	Container   *Container               `json:"container,omitempty"`
+	Files       []File                   `json:"files,omitempty"`
+	Filesets    []map[string]interface{} `json:"filesets,omitempty"`
+	Webcaptures []map[string]interface{} `json:"webcaptures,omitempty"`
+	ContainerId string                   `json:"container_id,omitempty"`
+	Contribs    []Contrib                `json:"contribs,omitempty"`
+	ExtIDs      ExtID                    `json:"ext_ids,omitempty"`
 	Extra       struct {
 		Arxiv struct {
 			Subjects []string `json:"subjects,omitempty"`
@@ -114,10 +129,10 @@ type Release struct {
 	Pages           string `json:"pages,omitempty"`
 	Publisher       string `json:"publisher,omitempty"`
 	Refs            []Ref  `json:"refs,omitempty"`
-	ReleaseDate     string `json:"release_date,omitempty"`
+	ReleaseDate     string `json:"release_date,omitempty"` // todo: use *time.Time?
 	ReleaseStage    string `json:"release_stage,omitempty"`
 	ReleaseType     string `json:"release_type,omitempty"`
-	ReleaseYear     int64  `json:"release_year,omitempty"`
+	ReleaseYear     int64  `json:"release_year,omitempty"` // todo: pointer for null?
 	Revision        string `json:"revision,omitempty"`
 	State           string `json:"state,omitempty"`
 	Subtitle        string `json:"subtitle,omitempty"`
@@ -125,7 +140,35 @@ type Release struct {
 	Version         string `json:"version,omitempty"`
 	Volume          string `json:"volume,omitempty"`
 	WithdrawnStatus string `json:"withdrawn_status,omitempty"`
-	WithdrawnDate   string `json:"withdrawn_date,omitempty"`
+	WithdrawnDate   string `json:"withdrawn_date,omitempty"` // todo: use time.Time?
 	WithdrawnYear   string `json:"withdrawn_year,omitempty"`
 	WorkID          string `json:"work_id,omitempty"` // work ident
+}
+
+type Container struct {
+	Name              string `json:"name,omitempty"`
+	ContainerType     string `json:"container_type,omitempty"`
+	PublicationStatus string `json:"publication_status,omitempty"`
+	Publisher         string `json:"publisher,omitempty"`
+	Issnl             string `json:"issnl,omitempty"`
+	Issne             string `json:"issne,omitempty"`
+	Issnp             string `json:"issnp,omitempty"`
+	WikidataQid       string `json:"wikidata_qid,omitempty"`
+}
+
+type FileUrl struct {
+	Url string `json:"url,omitempty"`
+	Rel string `json:"rel,omitempty"`
+}
+
+type File struct {
+	Size         int64     `json:"size,omitempty"` // todo: nullable?
+	Md5          string    `json:"md5,omitempty"`
+	Sha1         string    `json:"sha1,omitempty"`
+	Sha256       string    `json:"sha256,omitempty"`
+	Urls         []FileUrl `json:"urls,omitempty"`
+	Mimetype     string    `json:"mimetype,omitempty"`
+	ContentScope string    `json:"content_scope,omitempty"`
+	ReleaseIds   []string  `json:"release_ids,omitempty"`
+	Releases     []Release `json:"releases,omitempty"` // Note: This creates a circular reference
 }
